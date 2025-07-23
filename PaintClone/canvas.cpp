111c8151be74be7bf33bf6edde0ce59f74b3f990
MaximilianMcC <max@maximilian.co.nz>
2025-07-24 03:38:30 +1200

    temp drawing idk

--- PaintClone/canvas.cpp
+++ PaintClone/canvas.cpp
@@ -1,5 +1,4 @@
 #include "canvas.h"
-#include <iostream>
 
 Canvas::Canvas()
 {
@@ -14,7 +13,7 @@ void Canvas::Start()
 		Program::GetWindow()->getSize().y * 0.8f,
 	});
 
-	// Set a default background color of a kinda dark color
+	// Set a default background color
 	// TODO: Set transparent with transparent grid in the back
 	// TODO: Put colors and whatnot in enum
 	canvas.setFillColor(sf::Color(0xff00ffff));
@@ -23,15 +22,17 @@ void Canvas::Start()
 	//? When you add layers, just draw in the correct order instead of multiple render textures
 	sf::Vector2u canvasSize = static_cast<sf::Vector2u>(canvas.getSize());
 	renderTexture = sf::RenderTexture(canvasSize);
-
+	
 	// Make the sprite so we can actually render the render texture
+	renderTexture.display();
 	outputSprite = sf::Sprite(renderTexture.getTexture());
 
 	// Figure out how much room we've for the canvas to
 	// occupy, excluding the left and top bar ui things
+	// TODO: Add ui
 	sf::Vector2f availableSpace = static_cast<sf::Vector2f>(Program::GetWindow()->getSize());
-	// availableSpace.X -= leftToolBarSize.X;
-	// availableSpace.Y -= leftToolBarSize.Y;
+	//! availableSpace.X -= leftToolBarSize.X;
+	//! availableSpace.Y -= topToolBarSize.Y;
 	
 	// Place the canvas in the centre by default/initially
 	// TODO: Account for zoom → `outputSprite.setScale();`
@@ -41,19 +42,27 @@ void Canvas::Start()
 
 void Canvas::Update()
 {
+	//! debug
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+	{
+		// Get the position of the mouse
+		sf::Vector2f position;
+		if (GetMousePosition(position) == false) return;
 
+		// Make the dot
+		sf::CircleShape stroke = sf::CircleShape();
+		stroke.setRadius(30);
+		stroke.setPosition(position);
+
+		// Draw the dot
+		renderTexture.draw(stroke);
+	}
 }
 
 void Canvas::Draw()
 {
-	//! temp debug
-	renderTexture.draw(canvas);
-
-	// Turn the render texture into a sprite so we can draw it
-	// sf::Texture canvas = renderTexture.getTexture();
-	// sf::Sprite canvasSprite(canvas);
-
-	// Program::GetWindow()->draw(canvasSprite);
+	// Draw the canvas
+	Program::GetWindow()->draw(*outputSprite);
 }
 
 //? & is a fake c# out variable
