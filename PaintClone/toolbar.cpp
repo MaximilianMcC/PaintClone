d349be6ed3ea4409018bc5406863d571bbe32497
MaximilianMcC <max@maximilian.co.nz>
2025-07-30 13:59:28 +1200

    Panel things render correctly idk

--- PaintClone/toolbar.cpp
+++ PaintClone/toolbar.cpp
@@ -7,10 +7,11 @@ Toolbar::Toolbar()
 
 void Toolbar::Start()
 {
-	// The 'size' of a toolbar is 1/20th of the screen size
+	// The 'size' of a toolbar is x% of the screen size
+	//? idk what percentage it actually is (bad at maths)
 	// TODO: No magic numbers
 	sf::Vector2u screenSize = sf::VideoMode::getDesktopMode().size;
-	float size = screenSize.x / 20.0f;
+	float size = screenSize.x / 30.0f;
 
 	// Horizontal
 	toolSettings = sf::RectangleShape(sf::Vector2(0.0f, size));
@@ -20,9 +21,9 @@ void Toolbar::Start()
 	toolSelector.setPosition(sf::Vector2(0.0f, size));
 
 	// Set the colors
-	const sf::Color backgroundColor = sf::Color(0x333333FF);
-	toolSettings.setFillColor(backgroundColor);
-	toolSelector.setFillColor(backgroundColor);
+	// TODO: Color themes
+	toolSettings.setFillColor(sf::Color(0x222222FF)); // Darker gray
+	toolSelector.setFillColor(sf::Color(0x333333FF)); // Dark gray
 
 	// Set their initial dynamic sizes
 	ResizeUi();
@@ -41,12 +42,17 @@ void Toolbar::ResizeUi()
 	// Get the new window size
 	sf::Vector2u windowSize = Program::GetWindow()->getSize();
 
-	// Set the horizontal one to be the entire screen width
-	toolSelector.setSize(sf::Vector2f(toolSelector.getSize().x, windowSize.y));
+	// The top one's width is the same as the window
+	toolSettings.setSize(sf::Vector2f(
+		(float)windowSize.x,
+		toolSettings.getSize().y)
+	);
 
-	// Set the vertical thingy to be the entire
-	// screen height, minus the top of the settings
-	toolSettings.setSize(sf::Vector2f(toolSettings.getSize().x, windowSize.y - toolSelector.getSize().y));
+	// The side ones height is the same as the window
+	toolSelector.setSize(sf::Vector2f(
+		toolSelector.getSize().x,
+		(float)windowSize.y
+	));
 }
 
 void Toolbar::Draw()
