310c981dbf270bb222732720413045fc4631e6c9
MaximilianMcC <max@maximilian.co.nz>
2025-08-13 01:22:40 +1200

    added ui background color

--- PaintClone/colors.h
+++ PaintClone/colors.h
@@ -7,6 +7,7 @@ struct ColorTheme
 	sf::Color BackgroundDark;
 	sf::Color BackgroundLight;
 	sf::Color Foreground;
+	sf::Color UiBackground;
 };
 
 struct Colors {
@@ -15,19 +16,22 @@ struct Colors {
 	inline static const ColorTheme Light = {
 		sf::Color(0xf0f0f0FF),
 		sf::Color(0xdcdcdcFF),
-		sf::Color::Black
+		sf::Color::Black,
+		sf::Color(0xa0a0a0FF)
 	};
 
 	inline static const ColorTheme Dark = {
 		sf::Color(0x333333FF),
 		sf::Color(0x222222FF),
-		sf::Color::White
+		sf::Color::White,
+		sf::Color(0xa0a0a0FF)
 	};
 
 	// TODO: Crazy shortcut to activate theme
 	inline static const ColorTheme HotdogStand = {
 		sf::Color(0xff0000FF),
 		sf::Color(0xffff00FF),
-		sf::Color::Black
+		sf::Color::Black,
+		sf::Color(0xa0a0a0FF)
 	};
 };
