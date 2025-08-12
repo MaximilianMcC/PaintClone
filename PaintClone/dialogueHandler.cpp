bd0eb948535671717107cad94b257fbb7649b761
MaximilianMcC <max@maximilian.co.nz>
2025-08-12 14:51:57 +1200

    Save and load file dialogues

--- PaintClone/dialogueHandler.cpp
+++ PaintClone/dialogueHandler.cpp
@@ -49,3 +49,64 @@ sf::Color DialogueHandler::LaunchColorPicker(sf::Color previousColor)
 	if (ChooseColor(&colorPicker)) return previousColor;
 	return Utils::Win32ColorToSfml(colorPicker.rgbResult);
 }
+
+//? the A on the end means it uses ascii
+//? Not using a std::string so we dont lose it when returning
+OPENFILENAMEA DialogueHandler::MakeFileDialogue(const char* title)
+{
+	// Allocate space for the dialogue
+	OPENFILENAMEA fileDialogue;
+	ZeroMemory(&fileDialogue, sizeof(fileDialogue));
+	fileDialogue.lStructSize = sizeof(fileDialogue);
+
+	// Link the window to us
+	fileDialogue.hwndOwner = Utils::GetWindow()->getNativeHandle();
+
+	// Set the title
+	fileDialogue.lpstrTitle = title;
+
+	// Get the path to the default photos directory
+	//? static means it stays inside the object once returned
+	static char defaultPath[MAX_PATH];
+	SHGetFolderPathA(NULL, CSIDL_MYPICTURES, NULL, 0, defaultPath);
+
+	// Set the default filename
+	strcat_s(defaultPath, "\\MyPicture.png");
+
+	// Set the default path
+	fileDialogue.lpstrFile = defaultPath;
+	fileDialogue.nMaxFile = MAX_PATH;
+
+	// Make and set the filter things
+	fileDialogue.lpstrFilter =
+		"Anything\0*.*\0"
+		"Images and whatnot\0*.png;*.jpg;*.jpeg;*.bmp\0"
+		"\0";
+	fileDialogue.nFilterIndex = 2;
+
+	// set the dialogues display/settings flags
+	fileDialogue.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT;
+
+	// Give back the dialogue
+	return fileDialogue;
+}
+
+std::string DialogueHandler::LaunchSaveFileDialogue()
+{	
+	// Make the dialogue
+	OPENFILENAMEA fileDialogue = MakeFileDialogue("Where are we saving the file rn");
+
+	// Return the selected path, otherwise an empty string
+	if (GetSaveFileNameA(&fileDialogue)) return std::string(fileDialogue.lpstrFile);
+	return "";
+}
+
+std::string DialogueHandler::LaunchOpenFileDialogue()
+{	
+	// Make the dialogue
+	OPENFILENAMEA fileDialogue = MakeFileDialogue("Where are we loading the file from rn");
+
+	// Return the selected path, otherwise an empty string
+	if (GetOpenFileNameA(&fileDialogue)) return std::string(fileDialogue.lpstrFile);
+	return "";
+}
--- PaintClone/dialogueHandler.h
+++ PaintClone/dialogueHandler.h
@@ -2,14 +2,21 @@
 #include <string>
 #include <Windows.h>
 #include <commdlg.h>
+#include <shlobj.h>
 #include "utils.h"
 
 class DialogueHandler
 {
+private:
+    static OPENFILENAMEA MakeFileDialogue(const char* title);
+
 public:
 
 	//? Will return true for save, false for not save
 	static bool ConfirmSaveMessageBox();
 	
 	static sf::Color LaunchColorPicker(sf::Color previousColor);
+
+	static std::string LaunchOpenFileDialogue();
+	static std::string LaunchSaveFileDialogue();
 };
