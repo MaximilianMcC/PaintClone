3957706b5d4be437d4545e3dd501155d0a29e3dc
MaximilianMcC <max@maximilian.co.nz>
2025-08-12 13:06:17 +1200

    color picker

--- PaintClone/dialogueHandler.cpp
+++ PaintClone/dialogueHandler.cpp
@@ -2,15 +2,50 @@
 
 bool DialogueHandler::ConfirmSaveMessageBox()
 {
-	// TODO: Use the SFML window handle
-	HWND window = nullptr;
-
 	int result = MessageBox(
-		window,
+		Utils::GetWindow()->getNativeHandle(),
 		L"surely you're saving rn",
 		L"erhm",
 		MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2
 	);
 
 	return result == IDYES;
+}
+
+sf::Color DialogueHandler::LaunchColorPicker(sf::Color previousColor)
+{
+	// The resultant color thing
+	CHOOSECOLOR colorPicker;
+
+	// All of the sfml default colors
+	// TODO: Maybe don't put here
+	static COLORREF presetColors[10] = {
+		Utils::sfmlColorToWin32(sf::Color::White),
+		Utils::sfmlColorToWin32(sf::Color::Black),
+		Utils::sfmlColorToWin32(sf::Color::Blue),
+		Utils::sfmlColorToWin32(sf::Color::Cyan),
+		Utils::sfmlColorToWin32(sf::Color::Green),
+		Utils::sfmlColorToWin32(sf::Color::Cyan),
+		Utils::sfmlColorToWin32(sf::Color::Green),
+		Utils::sfmlColorToWin32(sf::Color::Magenta),
+		Utils::sfmlColorToWin32(sf::Color::Red),
+		Utils::sfmlColorToWin32(sf::Color::Yellow)
+	};
+
+	// Allocate space for the color picker or something
+	ZeroMemory(&colorPicker, sizeof(colorPicker));
+	colorPicker.lStructSize = sizeof(colorPicker);
+	
+	// Set the default color to be whatever our previous
+	// color was, and add all the default SFML colors
+	colorPicker.lpCustColors = presetColors;
+	colorPicker.rgbResult = Utils::sfmlColorToWin32(previousColor);
+	
+	// Make it a part of this window, and let us make any color
+	colorPicker.hwndOwner = Utils::GetWindow()->getNativeHandle();
+	colorPicker.Flags = CC_FULLOPEN | CC_RGBINIT | CC_ANYCOLOR | CC_PREVENTFULLOPEN;
+
+	// Return the selected color, otherwise whatever the previous one was
+	if (ChooseColor(&colorPicker)) return previousColor;
+	return Utils::Win32ColorToSfml(colorPicker.rgbResult);
 }
--- PaintClone/dialogueHandler.h
+++ PaintClone/dialogueHandler.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <string>
 #include <Windows.h>
+#include <commdlg.h>
 #include "utils.h"
 
 class DialogueHandler
 {
 public:
 
-    //? Will return true for save, false for not save
-    static bool ConfirmSaveMessageBox();
+	//? Will return true for save, false for not save
+	static bool ConfirmSaveMessageBox();
+	
+	static sf::Color LaunchColorPicker(sf::Color previousColor);
 };
