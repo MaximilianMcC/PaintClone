6fa73ae2256dc26a8fdfc7aa9709bcbd7ef91925
MaximilianMcC <max@maximilian.co.nz>
2025-08-13 09:18:15 +1200

    actual line tool now runs

--- PaintClone/lineTool.cpp
+++ PaintClone/lineTool.cpp
@@ -4,7 +4,7 @@
 void LineTool::Start()
 {
 	// Make the slider (settings)
-	slider = Slider(&thickness, sf::Vector2f(100, 100), 200.0f, "erhm");
+	slider = Slider(&thickness, sf::Vector2f(30, 20), 200.0f, "erhm");
 }
 
 void LineTool::MakeNewShape()
@@ -24,16 +24,18 @@ void LineTool::ResizeShape(sf::Vector2f newSize)
 	float angle = atan2(newSize.y, newSize.x);
 
 	// Update the rectangle to make it a fake line
-	line->setSize(sf::Vector2f(length, thickness));
+	line->setSize(sf::Vector2f(length, thickness * 20));
 	line->setRotation(sf::radians(angle));
 }
 
 void LineTool::Update()
 {
 	slider.Update();
+	ShapeTool::Update();
 }
 
 void LineTool::Draw()
 {
+	ShapeTool::Draw();
 	slider.Draw();
 }
